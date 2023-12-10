b8758ea313494119833e640309cf06335038932d
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-10 16:16:10 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,26 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+// NavBar.js
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <nav className="navbar"> {/* Added the className */}
+      <ul> {/* Added the unordered list */}
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors">Directors</NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors">Actors</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
